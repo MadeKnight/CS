da280e7a45736a68ce413f29f3be72719e6509cd
MadeKnight <142689532+MadeKnight@users.noreply.github.com>
2023-09-16 16:34:37 +0300

    приветствие

---
+++ CS_nameint/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("ВВедите имя ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "андрей")
+{
+    Console.WriteLine("а, это снова ты ");
+}
+else
+{
+    Console.Write("Прив ");
+    Console.WriteLine(username);
+}
